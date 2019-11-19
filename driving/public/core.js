d67bc9c9f22b4d4bd8bde2246b9865a5c94061c1
Be3N2 <2017WakefieldB@gmail.com>
2019-11-19 12:55:02 -0500

    Working line graph

--- driving/public/core.js
+++ driving/public/core.js
@@ -1,30 +1,80 @@
 const w = 500;
-const h = 100;
+const h = 200;
 
-const rectWidth = 50;
-const rectHeight = 50;
+const rectWidth = 10;
+const rectHeight = 10;
 
 const svg = d3.select("body")
               .append("svg")
               .attr("width", w)
-              .attr("height", h);
+              .attr("height", h)
+              .attr("class","chart");
 
-const dataArr = [];
-d3.csv("./assets/ModifiedData.csv", function(data) {
-	dataArr = data.map(function(d) {
-		return [+d["Fail Time"], +d.Speed, +d.Brake, +d.Gas, +d.Steering, +d.X-Position, +d.Z-Position];
-	});
-});
+d3.request("./assets/ModifiedData.csv")
+		.mimeType("text/csv")
+		.response(function (xhr) {
+			//console.log (d3.csvParse(xhr.responseText)); 
+			var data = d3.csvParse(xhr.responseText);
+			var formattedArr = [];
+			data.forEach((d) => {
+				var formatted = [+d["Fail Time"], +d.Speed, +d.Brake, +d.Gas, +d.Steering, +d["X-Position"], +d["Z-Position"]];
+				formattedArr.push(formatted);
+			});
+			return formattedArr;
+		})
+		.get(function(data) {
+			console.log(data);
 
-//scale - get out of json format
+			var maxSpeed = d3.max(data, datum => datum[1]);
+			console.log(maxSpeed);
+			var yScale = d3.scaleLinear()
+				.domain([0, maxSpeed])
+				.range([h-20, 0]);
+			
+			var yAxis = d3.axisLeft()
+					.scale(yScale);
+			
+			var xAxis = d3.scaleLinear()
+			      .domain([0, data.length])
+			      .range([20, w]);
 
-svg.select('rect')
-  	.data(dataArr)
-	.enter() 
-	.append('rect')
-	.attr('x', (d,i) => i)
-	.attr('y', (d,i) => d[2])
-	.attr('width', rectWidth)
-	.attr('height', rectHeight)
-	.attr('fill', 'blue')
-	.attr('stroke', '#fff');
+			svg.append("g")
+			      .attr("transform", "translate(0," + h + ")")
+			      .call(d3.axisBottom(xAxis));
+
+			d3.select('svg')
+				.append('g')
+				.attr('transform', 'translate(20,10)')
+				.call(yAxis);
+
+			svg.append("path")
+		      .datum(data)
+		      .attr("fill", "none")
+		      .attr("stroke", "steelblue")
+		      .attr("stroke-width", 1.5)
+		      .attr("d", d3.line()
+		        .x(function(d,i) {return xAxis(i) })
+		        .y(function(d) { return yScale(d[1]) })
+		    );
+			/*
+			svg.append("path")
+		      .datum(data)
+		      .attr("fill", "none")
+		      .attr("stroke", "red")
+		      .attr("stroke-width", 1.5)
+		      .attr("d", d3.line()
+		        .x(function(d,i) {return xAxis(i) })
+		        .y(function(d) { return yScale(d[1]) })
+		    );
+
+		    svg.append("path")
+		      .datum(data)
+		      .attr("fill", "none")
+		      .attr("stroke", "black")
+		      .attr("stroke-width", 1.5)
+		      .attr("d", d3.line()
+		        .x(function(d,i) {return xAxis(i) })
+		        .y(function(d) { return yScale(d[1]) })
+		    );
+		    */
+		});
